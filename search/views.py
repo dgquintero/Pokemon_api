812b35f0874bf8d0968987a63c0f8cefbb7082aa
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-20 21:35:18 -0500

    make changes on view file

--- search/views.py
+++ search/views.py
@@ -8,30 +8,32 @@ from rest_framework.response import Response
 from .models import Search
 from django.http import Http404
 
+# request to the pokemon api with all information abou the pokemon
+#def index(request):
+#    res = []
+#    for i in range(1, 806):
+#        resu = {}
+#        u = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(i)
+#        response = requests.get(u)
+#        if response:
+#            result = response.json()
 
-def index(self):
-    res = []
-    #range=964
-    for i in range(1, 806):
-        resu = {}
-        u = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(i)
-        response = requests.get(u)
-        if response:
-            result = response.json()
+#            resu.update({"id": i,
+#                        "name": result['forms'][0]['name'],
+#                        "height": result['height'],
+#                        "weight": result['weight'],})
+#    
+#            for stat in result['stats']:
+#                stats = {}
 
-            resu.update({"id": i,
-                        "name": result['forms'][0]['name'],
-                        "height": result['height'],
-                        "weight": result['weight'],})
-    
-            for stat in result['stats']:
-                stats = {}
-                #stats.append({"name_stat": name_stats})
-                resu.update({stat['stat']['name']: stat['base_stat']})
-        res.append(resu)
-        with open('data.json', 'w') as f:
-            json.dump(res, f)
+#               resu.update({stat['stat']['name']: stat['base_stat']})
+#        res.append(resu)
+#        with open('data.json', 'w') as f:
+#            json.dump(res, f)
             
+#    return render(request, 'search/index.html')
+
+def index(request):
     return render(request, 'search/index.html')
 
 class ListPokemon(APIView):
@@ -44,4 +46,4 @@ class ListPokemon(APIView):
     def get(self, request, name):
         search = self.get_object(name)
         search_json = "pokemon_api/api_data.json"
-        return Response(video_json.data)
+        return Response(search_json.data)
